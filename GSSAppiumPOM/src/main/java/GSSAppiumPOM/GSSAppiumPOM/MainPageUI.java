27c9c999b3b8acc74981479c3122c49e90b647a3
pravendrachauhan <pravendrachauhan@192.168.43.217>
2018-03-10 20:45:15 +0530

    second commit

--- GSSAppiumPOM/src/main/java/GSSAppiumPOM/GSSAppiumPOM/MainPageUI.java
+++ GSSAppiumPOM/src/main/java/GSSAppiumPOM/GSSAppiumPOM/MainPageUI.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class MainPageUI {
-	
 	WebDriver driver;
 	public MainPageUI(WebDriver driver) {
 		this.driver=driver;
